3c0d97e442b4f65b788a85d81cb807ef60ffff46
SHICHUAN <15180796292@163.com>
2016-12-12 14:43:44 +0800

    Initial Commit

---
+++ weiXinShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weiXinShare
+//
+//  Created by SHICHUAN on 2016/12/12.
+//  Copyright © 2016年 SHICHUAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ weiXinShare/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weiXinShare
+//
+//  Created by SHICHUAN on 2016/12/12.
+//  Copyright © 2016年 SHICHUAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
